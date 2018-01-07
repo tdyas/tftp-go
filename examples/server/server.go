2f9b7d22cf1600f823c4d02a4ba708b371384863
Tom Dyas <tom@shoalsoftware.com>
2018-01-07 14:37:38 -0500

    make file accesses pluggable
    Introduce the ability to configure the server to not access the local
    filesystem. The default implementation does access the config.ReadRoot
    and config.WriteRoot directories.
    
    This is mainly to support unit testing without the need to access
    the filesystem.

--- examples/server/server.go
+++ examples/server/server.go
@@ -15,8 +15,12 @@ func main() {
 
 	dir := os.Args[1]
 	log.Printf("root: %v", dir)
-	var config = tftp.ServerConfig{TracePackets: true}
-	server, err := tftp.NewServer(":9090", dir, &config)
+	var config = tftp.ServerConfig{
+		ReadRoot: dir,
+		WriteRoot: dir,
+		TracePackets: true,
+		}
+	server, err := tftp.NewServer(":9090", &config)
 	if err != nil {
 		log.Printf("Error: %v", err)
 		os.Exit(1)
--- server.go
+++ server.go
@@ -9,9 +9,13 @@ import (
 	"os"
 	"path"
 	"strconv"
+	"strings"
 )
 
 var RootMustBeAADirectoryError = errors.New("the TFTP root must be a directory")
+var FileNotFoundError = errors.New("File not found")
+var FileExistsError = errors.New("File already exists")
+var OperationNotSupportedError = errors.New("Operation is not supported.")
 
 func readExact(reader io.Reader, buffer []byte) (int, error) {
 	n := 0
@@ -35,12 +39,15 @@ type ServerConfig struct {
 	MaxBlockSize   uint16
 	DisableOptions bool
 	TracePackets   bool
+	ReadRoot       string
+	WriteRoot      string
+	GetReadStream  func(filename string) (io.ReadCloser, int64, error)
+	GetWriteStream func(filename string) (io.WriteCloser, error)
 }
 
 type Server struct {
 	config *ServerConfig
 	conn   net.PacketConn
-	root   string
 	log    *log.Logger
 	done   chan bool
 }
@@ -48,21 +55,62 @@ type Server struct {
 func (server *Server) handleRRQ(state *connectionState, request *ReadRequest, replyConn net.PacketConn, remoteAddr net.Addr) {
 	server.log.Printf("RRQ(%v): file=%v mode=%v", remoteAddr, request.Filename, request.Mode)
 
-	// Ensure the file exists
-	fullPath := path.Join(server.root, request.Filename)
-	stat, err := os.Stat(fullPath)
-	if err != nil || stat.IsDir() {
-		state.send(&Error{Code: ERR_FILE_NOT_FOUND, Message: "File not found"})
-		return
-	}
-	server.log.Printf("fullPath=%v, stat=%#v", fullPath, stat)
+	// Default implementation of GetReadStream.
+	getReadStream := func(filename string) (io.ReadCloser, int64, error) {
+		// Only allow reads if there is a ReadRoot configured.
+		if server.config.ReadRoot == "" {
+			return nil, -1, OperationNotSupportedError
+		}
 
-	f, err := os.Open(fullPath)
-	if err != nil {
-		state.send(&Error{Code: ERR_FILE_NOT_FOUND, Message: "File not found"})
-		return
+		// Security: Ensure that the filename is relative and does not contain any ../ references.
+		if path.IsAbs(filename) || strings.Contains(filename, "../") {
+			return nil, -1, FileNotFoundError
+		}
+
+		fullPath := path.Join(server.config.ReadRoot, filename)
+		stat, err := os.Stat(fullPath)
+		if err != nil {
+			if os.IsNotExist(err) || os.IsPermission(err) {
+				return nil, -1, FileNotFoundError
+			} else {
+				return nil, -1, err
+			}
+		}
+
+		if stat.IsDir() {
+			return nil, -1, FileNotFoundError
+		}
+
+		stream, err := os.Open(fullPath)
+		if err != nil {
+			if os.IsNotExist(err) || os.IsPermission(err) {
+				return nil, -1, FileNotFoundError
+			} else {
+				return nil, -1, err
+			}
+		}
+
+		return stream, stat.Size(), nil
 	}
-	defer f.Close()
+
+	if server.config.GetReadStream != nil {
+		getReadStream = server.config.GetReadStream
+	}
+
+	// Ask the GetReadStream function for access to the file.
+	stream, streamSize, err := getReadStream(request.Filename)
+	if err != nil {
+		if err == FileNotFoundError || err == OperationNotSupportedError {
+			state.send(&Error{Code: ERR_FILE_NOT_FOUND, Message: "File not found"})
+			return
+		} else {
+			state.send(&Error{Code: ERR_NOT_DEFINED, Message: "Unable to read file"})
+			return
+		}
+	}
+
+	// Ensure the stream is closed no matter how this function returns.
+	defer stream.Close()
 
 	var hasBlockSizeOption = false
 	if requestedBlockSizeString, ok := request.Options["blksize"]; ok {
@@ -103,8 +151,8 @@ func (server *Server) handleRRQ(state *connectionState, request *ReadRequest, re
 		if hasBlockSizeOption {
 			oack.Options["blksize"] = strconv.Itoa(int(state.blockSize))
 		}
-		if hasTransferSizeOption {
-			oack.Options["tsize"] = strconv.FormatInt(stat.Size(), 10)
+		if hasTransferSizeOption && streamSize != -1 {
+			oack.Options["tsize"] = strconv.FormatInt(streamSize, 10)
 		}
 		if hasTimeoutOption {
 			oack.Options["timeout"] = strconv.Itoa(state.timeout)
@@ -148,7 +196,7 @@ func (server *Server) handleRRQ(state *connectionState, request *ReadRequest, re
 	var blockNum uint16 = 1
 fileSend:
 	for {
-		n, err := readExact(f, fileBuffer)
+		n, err := readExact(stream, fileBuffer)
 		if err != nil {
 			server.log.Printf("readExact failed with: %v", err)
 			return
@@ -202,21 +250,57 @@ fileSend:
 func (server *Server) handleWRQ(state *connectionState, request *WriteRequest, replyConn net.PacketConn, remoteAddr net.Addr) {
 	server.log.Printf("WRQ(%v): file=%v mode=%v", remoteAddr, request.Filename, request.Mode)
 
-	// Ensure the file does not already exist.
-	fullPath := path.Join(server.root, request.Filename)
-	stat, err := os.Stat(fullPath)
-	if err == nil {
-		state.send(&Error{Code: ERR_FILE_EXISTS, Message: "File already exists"})
-		return
-	}
-	server.log.Printf("fullPath=%v, stat=%#v", fullPath, stat)
+	// Default implementation of GetWriteStream.
+	getWriteStream := func(filename string) (io.WriteCloser, error) {
+		// Only allow writes if there is a ReadRoot configured.
+		if server.config.WriteRoot == "" {
+			return nil, OperationNotSupportedError
+		}
 
-	f, err := os.OpenFile(fullPath, os.O_WRONLY|os.O_CREATE, 0666)
-	if err != nil {
-		state.send(&Error{Code: ERR_NOT_DEFINED, Message: "Unable to create file"})
-		return
+		// Security: Ensure that the filename is relative and does not contain any .. references.
+		if path.IsAbs(filename) || strings.Contains(filename, "../") {
+			return nil, FileExistsError
+		}
+
+		fullPath := path.Join(server.config.WriteRoot, filename)
+		_, err := os.Stat(fullPath)
+		if err == nil {
+			return nil, FileExistsError
+		}
+
+		stream, err := os.OpenFile(fullPath, os.O_WRONLY|os.O_CREATE, 0666)
+		if err != nil {
+			if os.IsExist(err) {
+				return nil, FileExistsError
+			} else {
+				return nil, err
+			}
+		}
+
+		return stream, nil
 	}
-	defer f.Close()
+
+	if server.config.GetWriteStream != nil {
+		getWriteStream = server.config.GetWriteStream
+	}
+
+	// Ask the GetReadStream function for access to the file.
+	stream, err := getWriteStream(request.Filename)
+	if err != nil {
+		if err == FileExistsError {
+			state.send(&Error{Code: ERR_FILE_EXISTS, Message: "File already exists"})
+			return
+		} else if err == OperationNotSupportedError {
+			state.send(&Error{Code: ERR_ILLEGAL_OPERATION, Message: "Write requests are not supported."})
+			return
+		} else {
+			state.send(&Error{Code: ERR_NOT_DEFINED, Message: "Unable to write to file"})
+			return
+		}
+	}
+
+	// Ensure the stream is closed no matter how this function returns.
+	defer stream.Close()
 
 	var hasBlockSizeOption = false
 	if requestedBlockSizeString, ok := request.Options["blksize"]; ok {
@@ -338,7 +422,7 @@ func (server *Server) handleWRQ(state *connectionState, request *WriteRequest, r
 dataBlockLoop:
 	for {
 		// Write the next data packet to the file.
-		f.Write(nextDataPacket.Data)
+		stream.Write(nextDataPacket.Data)
 
 		// Send an acknowledgement to the client.
 		ack := Ack{Block: blockNum}
@@ -435,7 +519,7 @@ func (server *Server) mainServerLoop() {
 	server.done <- true
 }
 
-func validateServerConfig(config *ServerConfig) *ServerConfig {
+func validateServerConfig(config *ServerConfig) (*ServerConfig, error) {
 	if config == nil {
 		config = &ServerConfig{}
 	}
@@ -450,29 +534,43 @@ func validateServerConfig(config *ServerConfig) *ServerConfig {
 		panic("MaxBlockSize must be between 8 and 65464 inclusive.")
 	}
 
-	return config
+	if config.ReadRoot != "" {
+		stat, err := os.Stat(config.ReadRoot)
+		if err != nil {
+			return nil, err
+		}
+		if !stat.IsDir() {
+			return nil, RootMustBeAADirectoryError
+		}
+	}
+
+	if config.WriteRoot != "" {
+		stat, err := os.Stat(config.WriteRoot)
+		if err != nil {
+			return nil, err
+		}
+		if !stat.IsDir() {
+			return nil, RootMustBeAADirectoryError
+		}
+	}
+
+	return config, nil
 }
 
-func NewServer(address string, root string, config *ServerConfig) (*Server, error) {
-	config = validateServerConfig(config)
+func NewServer(address string, config *ServerConfig) (*Server, error) {
+	config, err := validateServerConfig(config)
+	if err != nil {
+		return nil, err
+	}
 
 	conn, err := net.ListenPacket("udp", address)
 	if err != nil {
 		return nil, err
 	}
 
-	stat, err := os.Stat(root)
-	if err != nil {
-		return nil, err
-	}
-	if !stat.IsDir() {
-		return nil, RootMustBeAADirectoryError
-	}
-
 	server := Server{
 		config: config,
 		conn:   conn,
-		root:   root,
 		log:    log.New(os.Stderr, "TFTP: ", log.LstdFlags),
 		done:   make(chan bool, 1),
 	}
