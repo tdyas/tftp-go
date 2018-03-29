94f31de9e354da4301a974fc23d88f15cd0988c6
Tom Dyas <tom@shoalsoftware.com>
2018-03-29 11:06:04 -0700

    switch client GetFile to use `sendAndReceiveNext`

--- client.go
+++ client.go
@@ -24,6 +24,7 @@ func (e *TftpRemoteError) Error() string {
 
 type ClientConfig struct {
 	DisableOptions bool
+	MaxRetries     int
 	TracePackets   bool
 	Logger         *log.Logger
 }
@@ -32,6 +33,9 @@ func validateClientConfig(userConfig *ClientConfig) (*ClientConfig, error) {
 	var config = *userConfig
 	if config.Logger == nil {
 		config.Logger = log.New(ioutil.Discard, "", 0)
+	}
+	if config.MaxRetries == 0 {
+		config.MaxRetries = 5
 	}
 	return &config, nil
 }
@@ -79,6 +83,7 @@ func GetFile(
 		mainRemoteAddr: mainRemoteAddr,
 		blockSize:      DEFAULT_BLOCKSIZE,
 		timeout:        5,
+		maxRetries:     config.MaxRetries,
 		tracePackets:   config.TracePackets,
 	}
 
@@ -106,110 +111,95 @@ func GetFile(
 	var actualTransferSize uint64 = 0
 	var expectedTransferSize uint64 = 0
 
-rrqLoop:
-	for {
-		state.send(&rrq)
-
-		// Wait for the reply packet. The `receive` method will automatically latch onto to the IP address
-		// of the first reply packet to return.
-		rawPacket, err := state.receive()
-		if err != nil {
-			if timeoutError, ok := err.(net.Error); ok && timeoutError.Timeout() {
-				continue rrqLoop
-			}
-			return err
-		}
-
+	rawPacket, err := state.sendAndReceiveNext(&rrq, func(rawPacket interface{}) (bool, error) {
 		switch packet := rawPacket.(type) {
 		case Data:
-			if len(rrq.Options) > 0 {
-				state.log("TFTP server %v does not support custom options.", mainRemoteAddr)
-			}
-			if packet.Block != currentBlockNum {
-				log.Printf("TFTP server sent unexpected block number.")
-				state.send(&Error{Code: ERR_ILLEGAL_OPERATION, Message: "Illegal operation."})
-				return TftpProtocolViolation
-			}
-			currentDataPacket = &packet
-			writer.Write(packet.Data)
-			actualTransferSize += uint64(len(packet.Data))
-			break rrqLoop
-
+			return true, nil
 		case OptionsAck:
-			// Parse the options to understand what the server did and did not accept.
-			if value, ok := packet.Options["blksize"]; ok {
-				accetpedBlockSize, err := strconv.Atoi(value)
-				if err == nil {
-					// TODO: validate the accepted blocksize
-					state.log("using block size = %v", accetpedBlockSize)
-					state.blockSize = uint16(accetpedBlockSize)
-				}
-			}
-			if value, ok := packet.Options["tsize"]; ok {
-				s, err := strconv.Atoi(value)
-				if err == nil {
-					expectedTransferSize = uint64(s)
-					state.log("expected transfer size: %d", expectedTransferSize)
-				}
-			}
-
-			// Reset the block number so that the data receive loop will send the correct acknowlegement.
-			currentBlockNum = 0
-			break rrqLoop
-
-		case Error:
-			// Log the error and terminate the connection.
-			state.log("Server returned error: %v", packet.Message)
-			return &TftpRemoteError{
-				Code:    int(packet.Code),
-				Message: packet.Message}
-
+			return true, nil
 		default:
 			state.log("Server returned unexpected packet type: %v", packet)
+			return false, TftpProtocolViolation
+		}
+	})
+	if err != nil {
+		return err
+	}
+
+	switch packet := rawPacket.(type) {
+	case Data:
+		if len(rrq.Options) > 0 {
+			state.log("TFTP server %v does not support custom options.", mainRemoteAddr)
+		}
+		if packet.Block != currentBlockNum {
+			log.Printf("TFTP server sent unexpected block number.")
+			state.send(&Error{Code: ERR_ILLEGAL_OPERATION, Message: "Illegal operation."})
 			return TftpProtocolViolation
 		}
+		currentDataPacket = &packet
+		writer.Write(packet.Data)
+		actualTransferSize += uint64(len(packet.Data))
+
+	case OptionsAck:
+		// Parse the options to understand what the server did and did not accept.
+		if value, ok := packet.Options["blksize"]; ok {
+			accetpedBlockSize, err := strconv.Atoi(value)
+			if err == nil {
+				// TODO: validate the accepted blocksize
+				state.log("using block size = %v", accetpedBlockSize)
+				state.blockSize = uint16(accetpedBlockSize)
+			}
+		}
+		if value, ok := packet.Options["tsize"]; ok {
+			s, err := strconv.Atoi(value)
+			if err == nil {
+				expectedTransferSize = uint64(s)
+				state.log("expected transfer size: %d", expectedTransferSize)
+			}
+		}
+
+		// Reset the block number so that the data receive loop will send the correct acknowledgement.
+		currentBlockNum = 0
 	}
 
 dataLoop:
 	for {
 		// Send the acknowledgement for the data packet or OACK.
 		ack := Ack{Block: currentBlockNum}
-		state.send(&ack)
 
 		// Transfer ends when we receive a packet with less data than the block size.
 		if currentDataPacket != nil && uint16(len(currentDataPacket.Data)) < state.blockSize {
+			state.send(&ack)
 			break dataLoop
 		}
 
-		rawPacket, err := state.receive()
-		if err != nil {
-			if timeoutError, ok := err.(net.Error); ok && timeoutError.Timeout() {
-				continue dataLoop
+		rawPacket, err := state.sendAndReceiveNext(&ack, func(rawPacket interface{}) (bool, error) {
+			switch packet := rawPacket.(type) {
+			case Data:
+				if packet.Block == currentBlockNum+1 {
+					return true, nil
+				} else if packet.Block <= currentBlockNum {
+					// Disregard prior DATA packets.
+					return false, nil
+				} else {
+					// Data packets past the current block violate the protocol.
+					return false, TftpProtocolViolation
+				}
+
+			default:
+				return false, TftpProtocolViolation
 			}
+
+		})
+		if err != nil {
 			return err
 		}
 
-		switch packet := rawPacket.(type) {
-		case Data:
-			if packet.Block == currentBlockNum+1 {
-				currentBlockNum = packet.Block
-				currentDataPacket = &packet
-				writer.Write(packet.Data)
-				actualTransferSize += uint64(len(packet.Data))
-				continue dataLoop
-			}
-
-		case Error:
-			// Log the error and terminate the connection.
-			state.log("Server returned error: %s", packet.Message)
-			return &TftpRemoteError{
-				Code:    int(packet.Code),
-				Message: packet.Message}
-
-		default:
-			state.log("Server returned unexpected packet type: %v", packet)
-			return TftpProtocolViolation
-		}
+		packet := rawPacket.(Data)
+		currentBlockNum = packet.Block
+		currentDataPacket = &packet
+		writer.Write(packet.Data)
+		actualTransferSize += uint64(len(packet.Data))
 	}
 
 	if expectedTransferSize != 0 && actualTransferSize != expectedTransferSize {
