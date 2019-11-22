8e594de776f4cd58c97182b3b5e2fea123698157
Tom Dyas <tom@shoalsoftware.com>
2019-11-22 14:08:18 -0800

    examples/client: add `put` support

--- examples/client/client.go
+++ examples/client/client.go
@@ -3,30 +3,70 @@ package main
 import (
 	"context"
 	"fmt"
+	tftp "github.com/tdyas/tftp-go"
 	"os"
-	"github.com/tdyas/tftp-go"
 )
 
+func get(address string, srcFilename string, destFilename string) error {
+	f, err := os.OpenFile(destFilename, os.O_WRONLY|os.O_CREATE, 0666)
+	if err != nil {
+		fmt.Printf("ERROR: Unable to create file %v: %v\n", destFilename, err)
+		return err
+	}
+	defer f.Close()
+
+	err = tftp.GetFile(context.Background(), address, srcFilename, "octet", nil, f)
+	if err != nil {
+		fmt.Printf("ERROR: TFTP read request failed: %v\n", err)
+		return err
+	}
+
+	return nil
+}
+
+func put(address string, srcFilename string, destFilename string) error {
+	f, err := os.Open(srcFilename)
+	if err != nil {
+		fmt.Printf("ERROR: Unable to open file `%s`: %v\n", srcFilename, err)
+		return err
+	}
+	defer f.Close()
+
+	err = tftp.PutFile(context.Background(), address, destFilename, "octet", nil, f)
+	if err != nil {
+		fmt.Printf("ERROR: TFTP write request failed: %v", err)
+		return err
+	}
+
+	return nil
+}
+
 func main() {
-	if len(os.Args) != 3 {
-		fmt.Printf("ERROR: Must specify address and file to retrieve.\n")
+	if len(os.Args) != 4 {
+		fmt.Printf("usage: read|write REMOTE SOURCE DEST.\n")
 		os.Exit(1)
 	}
 
 	address := os.Args[1]
-	filename := os.Args[2]
+	cmd := os.Args[2]
+	srcFilename := os.Args[3]
+	destFilename := os.Args[4]
 
-	file, err := os.OpenFile(filename, os.O_WRONLY | os.O_CREATE, 0666)
-	if err != nil {
-		fmt.Printf("ERROR: Unable to create file %v: %v\n", filename, err)
+	switch cmd {
+	case "get":
+		err := get(address, srcFilename, destFilename)
+		if err != nil {
+			os.Exit(1)
+		}
+
+	case "put":
+		err := put(address, srcFilename, destFilename)
+		if err != nil {
+			os.Exit(1)
+		}
+
+	default:
+		fmt.Printf("ERROR: unknown command `%s`\n", cmd)
 		os.Exit(1)
 	}
-
-	err = tftp.GetFile(context.Background(), address, filename, "octet", nil, file)
-	if err != nil {
-		fmt.Printf("ERROR: TFTP RRQ failed: %v\n", err)
-		os.Exit(1)
-	}
-
-	file.Close()
 }
